83deaab1a3fd4694127b8a7ea54fe7e58024b736
Zach Hornung <Zachhornung@comcast.net>
2021-01-27 16:11:49 -0800

    added local storage capability, improved botton functionality

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var imgContainer = document.getElementById('img-container');
 var leftPicImage = document.getElementById('left-pic');
 var centerPicImage = document.getElementById('center-pic');
 var rightPicImage = document.getElementById('right-pic');
-var roundsCounter = 0;
+var roundsCounter = fetchRoundsCount() || 0;
 var roundsLimit = 25;
 var listLocation = document.getElementById('list-here');
 var resultsDiv = document.getElementById('results'); //this is for when the max number of rounds is reached, a button will appear that will show results if you click on it
@@ -21,12 +21,13 @@ function ProductImage(name){ //this is the object constructor
   this.image = `images/${name}`;
   ProductImage.allImages.push(this);
 }
-ProductImage.allImages = []; // creates an allImages property on the ProductImage constructor.
+ProductImage.allImages = fetchProductData() || []; // creates an allImages property on the ProductImage constructor. its value is the product data stored in local memory, or an empty array if there is nothing stored in memory.
 
-for (var i = 0; i < imagesSourceArray.length; i++){ //this makes an object for each position in the image source array on line 3
-  new ProductImage(imagesSourceArray[i]);
+if (roundsCounter === 0){
+  for (var i = 0; i < imagesSourceArray.length; i++){ //this makes an object for each position in the image source array on line 3
+    new ProductImage(imagesSourceArray[i]);
+  }
 }
-
 function generateRandomProducts(){ //this generates a random number that ranges from 0 to however many index positions are in the product list. it then uses that number to select a random product form the array.
   var leftIndex = Math.floor(Math.random() * ProductImage.allImages.length);
   var centerIndex = Math.floor(Math.random() * ProductImage.allImages.length);
@@ -67,7 +68,37 @@ function renderProducts(){ //this renders the products to the screen with the co
   rightPicImage.name = newImages[2].name;
   newImages[2].timesShown++;
 }
-function forEventListener(event){ //this checks to see which image was clicked, and  then incriments the times clicked for that object by 1. it then increases the current voting round by 1, and generates new images. if the current voting round is the last voting round, it alerts the user, removes the event listener, and displays the results button to see the results.
+function storeProductData(){//stores all objects states in local storage as a string
+  var stringObject = JSON.stringify(ProductImage.allImages);
+  localStorage.setItem('products', stringObject);
+}
+function fetchProductData(){//converts all objects (which are stored as one long string) back into actual objects.
+  var reObjectify = localStorage.getItem('products');
+  var productsFromStorage = JSON.parse(reObjectify);
+  return productsFromStorage;
+}
+function storeRoundsCount(){//stores the number of voting rounds that have happened in local memory.
+  var storeRoundsCount = roundsCounter;
+  localStorage.setItem('rounds counter', storeRoundsCount);
+}
+function fetchRoundsCount(){//returns the stored rounds count from local memory.
+  var storedRoundsCount = parseInt(localStorage.getItem('rounds counter'));
+  return storedRoundsCount;
+}
+function forButtonClick(){// this is for the results button, when it is clicked it will display the results of the voting, and then remove the event listener from the button.
+  displayList();
+  displayChart();
+  button.removeEventListener('click', forButtonClick);
+}
+function forImgClick(event){//this is the event listener for the actual voting (when an image is clicked). it checks to make sure the rounds limit hasnt been met, and then finds the image that was clicked and adds a counter to it. it incriments to the next round, stores the current round in local memory, generates new products, and stores the data of those products in local memory.
+  event.preventDefault();
+  if (roundsCounter === roundsLimit || fetchRoundsCount() >= roundsLimit){
+    alert('That\'s ' + roundsLimit + ' Rounds of Voting!');
+    imgContainer.removeEventListener('click', forImgClick);
+    button.textContent = 'Results';
+    resultsDiv.appendChild(button);
+    return;
+  }
   for (var i = 0; i < ProductImage.allImages.length; i++){
     if (event.target.src.includes(ProductImage.allImages[i].image)){
       ProductImage.allImages[i].timesClicked++;
@@ -76,15 +107,12 @@ function forEventListener(event){ //this checks to see which image was clicked, 
   }
   // eslint-disable-next-line no-unused-vars
   roundsCounter++;
+  storeRoundsCount();
   console.log(roundsCounter);
+  console.log(fetchRoundsCount());
   var newProducts = generateRandomProducts();
   renderProducts(newProducts[0], newProducts[1], newProducts[2]);
-  if (roundsCounter === roundsLimit){
-    alert('That\'s ' + roundsLimit + ' Rounds of Voting!');
-    imgContainer.removeEventListener('click', forEventListener);
-    button.textContent = 'Results';
-    resultsDiv.appendChild(button);
-  }
+  storeProductData();
 }
 function displayList(){ //this creates a list that includes every item in the product catalog, showing the name of the item, how many times it was shown, and how many times it was clicked on.
   listLocation.appendChild(h2);
@@ -115,99 +143,15 @@ function displayChart(){ //this displays a chart that shows each item in the cat
         label: 'times clicked',
         data: votesByProduct, // array of numbers goes here
         // data: votesByProduct,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
+        backgroundColor: new Array(20).fill('rgba(1, 200, 32, 0.2)'),
+        borderColor: new Array(20).fill('rgba(1, 99, 32, 1)'),
         borderWidth: 1
       },
       {
         label: 'Times Shown',
         data: timesProductsAreShown,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-        ],
+        backgroundColor: new Array(20).fill('rgba(255, 200, 132, 0.2)'),
+        borderColor: new Array(20).fill('rgba(255, 99, 132, 1)'),
         borderWidth: 1
       }]
     },
@@ -225,10 +169,8 @@ function displayChart(){ //this displays a chart that shows each item in the cat
 var randomProducts = generateRandomProducts();
 renderProducts(randomProducts[0], randomProducts[1], randomProducts[2]);
 
-imgContainer.addEventListener('click', forEventListener);
+imgContainer.addEventListener('click', forImgClick);
 
-button.addEventListener('click', function (){ //this adds an event listener to the button that will display both the list and the chart when clicked.
-  displayList();
-  displayChart();
-});
-
+if (roundsCounter === roundsLimit){
+  button.addEventListener('click', forButtonClick);
+}
